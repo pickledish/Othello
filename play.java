1ea9addf0679fc912d86cbbddc6988e28928310f
Brandon Willett <bcwillett1@gmail.com>
2015-05-02 17:06:11 -0400

    play.sh

--- play.java
+++ play.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by Brandon on 4/18/15.
  *
@@ -7,15 +9,20 @@
 
 public class play {
 
-
 	public static void main(String[] args) {
 
 		boolean firstp = false;
-		if (args[0].equals("B"))
+
+		System.out.print("Please enter some arguments: ");
+		Scanner scanny = new Scanner(System.in);
+		String line = scanny.nextLine();
+		String[] arguments = line.split(" ");
+
+		if (arguments[0].equals("B"))
 			firstp = true;
-		String depthLimit = args[1];
-		String timeLimit1 = args[2];
-		String timeLimit2 = args[3];
+		String depthLimit = arguments[1];
+		String timeLimit1 = arguments[2];
+		String timeLimit2 = arguments[3];
 
 		GameWindow othello = new GameWindow(firstp);
 		othello.drawWindow();
